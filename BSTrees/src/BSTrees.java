bb818c62306b7430f90489d0fb0665e04c420c90
subedia <subedia@KRONOS3.ecs.csus.edu>
2015-03-05 17:12:05 -0800

    k

---
+++ BSTrees/src/BSTrees.java
@@ -0,0 +1,197 @@
+
+public class BSTrees {
+	public static void main(String[] args) {
+		BSTree tree = new BSTree();
+		tree.insert(4);
+		tree.insert(5);
+		tree.insert(6);
+		tree.inOrder();
+	}
+}
+
+class BSTree {
+
+	protected BSTNode rootnode; // define root node
+
+	class BSTNode {
+		public BSTNode left;
+		public BSTNode right;
+		public BSTNode root;
+		public int value;
+
+		public BSTNode(int k) {
+			left = right = root = null; // initially set left, right and root to
+										// null
+
+			value = k;
+		}
+
+		public String toString() {
+			return "" + value;
+		}
+	}
+
+	// Insert mehod
+	public void insert(int k) {
+		// create new node
+		BSTNode n = new BSTNode(k);
+		// recursively call insert mehod
+		insertBST(this.rootnode, n);
+	}
+
+	public void insertBST(BSTNode t, BSTNode x) {
+
+		if (t == null) {
+			this.rootnode = x;
+		} else {
+			if (x.value < t.value) // if entered value x < existing node value
+									// then go to left
+			{
+				if (t.left == null) {
+					t.left = x;
+					x.root = t;
+
+				} else {
+					insertBST(t.left, x);
+				}
+
+			} else if (x.value > t.value) // if entered value x > existing node
+											// value then go to right
+			{
+				if (t.right == null) {
+					t.right = x;
+					x.root = t;
+
+				} else {
+					insertBST(t.right, x);
+				}
+			} else {
+				x.root = t;
+
+			}
+		}
+	}
+
+	// method to remove node from BST
+	public void remove(int k) {
+		// First we must find the node, after this we can delete it.
+		removeBST(this.rootnode, k);
+	}
+
+	public void removeBST(BSTNode t, int x) {
+		if (t == null) {
+
+			return;
+		} else {
+			if (t.value > x) {
+				removeBST(t.left, x);
+			} else if (t.value < x) {
+				removeBST(t.right, x);
+			} else if (t.value == x) {
+				removeFoundNode(t);
+			}
+		}
+	}
+
+	public void removeFoundNode(BSTNode x) {
+		BSTNode r;
+
+		if (x.left == null || x.right == null) {
+			// the rootnode is deleted
+			if (x.root == null) {
+				this.rootnode = null;
+				x = null;
+				return;
+			}
+			r = x;
+		} else {
+
+			r = successor(x);
+			x.value = r.value;
+		}
+
+		BSTNode t;
+		if (r.left != null) {
+			t = r.left;
+		} else {
+			t = r.right;
+		}
+
+		if (t != null) {
+			t.root = r.root;
+		}
+
+		if (r.root == null) {
+			this.rootnode = t;
+		} else {
+			if (r == r.root.left) {
+				r.root.left = t;
+			} else {
+				r.root.right = t;
+			}
+		}
+		r = null;
+	}
+
+	// find the node in a way to violating node
+	public BSTNode successor(BSTNode x) {
+		if (x.right != null) {
+			BSTNode r = x.right;
+			while (r.left != null) {
+				r = r.left;
+			}
+			return r;
+		} else {
+			BSTNode t = x.root;
+			while (t != null && x == t.right) {
+				x = t;
+				t = x.root;
+			}
+			return t;
+		}
+	}
+
+	// Search a node
+	public void find(BSTNode n) {
+		int l = 0;
+		int r = 0;
+		int t = 0;
+		if (n.left != null) {
+			l = n.left.value;
+		}
+		if (n.right != null) {
+			r = n.right.value;
+		}
+		if (n.root != null) {
+			t = n.root.value;
+		}
+		if (n.left != null) {
+			find(n.left);
+		}
+		if (n.right != null) {
+			find(n.right);
+		}
+	}
+
+	// Returns tree in INORDER traversal
+	private int inOrder(BSTNode node) {
+		int returnThis=-1;
+		if (node != null) {
+			inOrder(node.left);
+			returnThis = node.value;
+//			System.out.println(node.value);
+			inOrder(node.right);
+		}
+		return returnThis;
+	}
+
+	void inOrder() {
+		inOrder(rootnode);
+	}
+
+	// Check for Tree is Empty or not
+
+	boolean isEmpty() {
+		return rootnode == null;
+	}
+}
